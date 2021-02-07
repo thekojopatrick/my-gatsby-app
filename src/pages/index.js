ad58a9cf26083c2054c3deafc03447c9a3309b5c
Kojo Patrick <patrickasare84@gmail.com>
2021-02-07 23:09:08 +0000

    Add Static Data Json

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,10 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from "../components/card"
 import Section from "../components/section"
+import Wave from "../components/wave"
+import staticdata from "../../staticdata.json"
+import Cell from "../components/cell"
+import styled from 'styled-components'
 
 //Importing Image Assets
 import sketch from "../images/logo-sketch.png"
@@ -21,8 +25,27 @@ import wallpaper3 from "../images/wallpaper3.png"
 import wallpaper4 from "../images/wallpaper4.png"
 import wallpaper5 from "../images/wallpaper5.png"
 import wallpaper6 from "../images/wallpaper6.png"
-import Wave from "../components/wave"
 
+
+const SectionCellGroup = styled.div`
+  max-width :800px;
+  margin: 0 auto 100px;
+  display:grid;
+  grid-template-columns:repeat(2,1fr);
+  grid-column-gap:20px;
+  padding: 0 20px;
+  
+  @media(max-width:800px){
+    grid-template-columns:repeat(1,1fr);
+  }
+`
+const SectionCaption = styled.p`
+  font-weight : 600;
+  font-size:18px;
+  text-transform:uppercase;
+  color:#94A4BA;
+  text-align :center;
+`
 
 
 const IndexPage = () => (
@@ -106,6 +129,17 @@ const IndexPage = () => (
       Contentful and deploying your site with Netlify."
     />
 
+    <SectionCaption>12 section - 6 hours</SectionCaption>
+    <SectionCellGroup>
+      {
+        staticdata.cells.map(cell => (
+          <Cell
+            title={cell.title}
+            image={cell.image}
+          />
+        ))
+      }
+    </SectionCellGroup>
   </Layout>
 )
 
